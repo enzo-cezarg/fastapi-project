a59dc200fc9afc731a044bd19ec123134d7388a4
Enzo Garcia <enzoocezargarcia@gmail.com>
2024-07-04 16:06:28 -0300

    first-endpoint-and-schemas-configuration

--- fastapi_project/app.py
+++ fastapi_project/app.py
@@ -1,8 +1,16 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fastapi_project.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
